2f3de1617f7d54f6bcb11c97a78f201989c6b90f
Haniye Hosseini <haniyehosseini.me@gmail.com>
2024-12-27 17:01:52 +0330

    implement generic repository

---
+++ TrackApi.Infrastructure/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TrackApi.Infrastructure.Context;
+using TrackItApi.Domain.Models;
+
+namespace TrackApi.Infrastructure.Repositories.Base
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseModel
+    {
+        private readonly TrackApiContext _context;
+        protected readonly DbSet<T> _dbSet;
+        public BaseRepository(TrackApiContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(long id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ TrackApi.Infrastructure/Repositories/Base/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TrackItApi.Domain.Models;
+
+namespace TrackApi.Infrastructure.Repositories.Base
+{
+    public interface IBaseRepository<T> where T : BaseModel
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(long id);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
